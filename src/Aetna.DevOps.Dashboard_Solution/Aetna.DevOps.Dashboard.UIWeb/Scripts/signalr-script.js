0fb32e45dcca4989e382002a1bfb2e60b4388740
Austin Barrett <austinbrrtt@gmail.com>
2017-11-08 17:11:11 -0500

    Added Javascript for SignalR

---
+++ src/Aetna.DevOps.Dashboard_Solution/Aetna.DevOps.Dashboard.UIWeb/Scripts/signalr-script.js
@@ -0,0 +1,11 @@
+﻿$().ready(function () {
+    var $deployAction = $.connection.deployAction;
+    $deployAction.client.onAction = onAction;
+    $.connection.hub.start();
+});
+function onAction(currentState) {
+    $("#startTime").text(currentState.StartTime);
+
+    $("#requestsProcessed").text(currentState.RequestsProcessed);
+    $("#bytesProcessed").text(currentState.BytesProcessed);
+}
