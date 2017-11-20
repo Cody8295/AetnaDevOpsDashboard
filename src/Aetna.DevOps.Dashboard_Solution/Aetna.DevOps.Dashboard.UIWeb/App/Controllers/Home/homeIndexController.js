70594745f2fab6f61290f94a441137a4e2f5c06a
Matt Rusczyk <mtrusczyk@gmail.com>
2017-11-20 10:48:58 -0500

    Home index now uses angular directives

--- src/Aetna.DevOps.Dashboard_Solution/Aetna.DevOps.Dashboard.UIWeb/App/Controllers/Home/homeIndexController.js
+++ src/Aetna.DevOps.Dashboard_Solution/Aetna.DevOps.Dashboard.UIWeb/App/Controllers/Home/homeIndexController.js
@@ -2,16 +2,51 @@
 
     var app = angular.module("app");
 
+    app.controller('projectGroupListController', function ($scope, $http) {
+        $http.get("/api/Octo/ProjectList").then(function (response) {
+            $scope.projectGroupList = response.data;
+        });
+    });
+
+    app.controller('environmentListController', function ($scope, $http) {
+        $http.get("/api/Octo/environmentList").then(function (response) {
+            $scope.environmentList = response.data;
+        });
+    });
+
+    app.controller('projectController', function ($scope, $http) {
+        $http.get("/api/Octo/projects").then(function (response) {
+            $scope.projects = response.data;
+        });
+    });
+
+    app.controller('projectGroupController', function ($scope, $http) {
+        $http.get("/api/Octo/projectGroups").then(function (response) {
+            $scope.projectGroups = response.data;
+        });
+    });
+
+    app.controller('environmentController', function ($scope, $http) {
+        $http.get("/api/Octo/environments").then(function (response) {
+            $scope.environments = response.data;
+        });
+    });
+
+    app.controller('lifecyclesController', function ($scope, $http) {
+        $http.get("/api/Octo/lifecycles").then(function (response) {
+            $scope.lifecycles = response.data;
+        });
+    });
     
     var homeIndexController = function ($scope, $http) {
-
+        /*
         $http.get("/api/Octo/projectGroups").then(function (response) {
             $(".projectGroups").replaceWith("<span class=\"pull-right\">" + response.data + "</span>");
         });
         $http.get("/api/Octo/projects").then(function (response) {
             $(".projects").replaceWith("<span class=\"pull-right\">" + response.data + "</span>");
         });
-
+        */
         function getReleases(projectName) {
             var releases = [];
             $http.get("/api/Octo/projectProgression?project=" + projectName).then(function (response) {
@@ -88,6 +123,7 @@
             });
         });
 
+        /*
         $http.get("/api/Octo/lifecycles").then(function (response) {
             $(".lifecycles").replaceWith("<span class=\"pull-right\">" + response.data + "</span>");
         });
@@ -104,7 +140,9 @@
             replace += "</ul></div>";
             $(".environments").replaceWith(replace);
         });
+        */
 
+        /*
         $http.get("/api/Octo/ProjectList").then(function(response) {
             var replace = "<div id=\"projectGroupList\" class=\"collapsible panel-collapse collapse\"><ul class=\"list-group\">";
             response.data.forEach(function (d) {
@@ -112,7 +150,7 @@
             })
             replace += "</ul></div>";
             $(".projectGroupList").replaceWith(replace);
-        });
+        }); */
         
         $http.get("/api/Octo/deploys").then(function (response) {
             $(document).ready(function () { // via https://stackoverflow.com/questions/9446318/bootstrap-tooltips-not-working
