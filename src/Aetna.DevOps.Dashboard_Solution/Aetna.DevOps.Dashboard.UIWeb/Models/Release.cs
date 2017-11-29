9c5596b992043091aa6b025e4f816a351910bc5d
Austin Barrett <austinbrrtt@gmail.com>
2017-11-28 23:50:56 -0500

    Added basic release notes parsing to Release.cs

--- src/Aetna.DevOps.Dashboard_Solution/Aetna.DevOps.Dashboard.UIWeb/Models/Release.cs
+++ src/Aetna.DevOps.Dashboard_Solution/Aetna.DevOps.Dashboard.UIWeb/Models/Release.cs
@@ -6,6 +6,14 @@ namespace Aetna.DevOps.Dashboard.UIWeb.Models
     {
         public string Id, Version, Projectid, Channelid, Assembled, Releasenotes, WebUrl;
         public List<ActiveDeploy> ReleaseDeploys;
+        public Dictionary<string, string> Details;
+        public enum Source
+        {
+            TFS,
+            TcTFS,
+            TcGHE
+        }
+        public Source ProjectSource;
 
         public Release(string id, string version, string projectId, string channelId, string assembled, string releaseNotes, List<ActiveDeploy> releaseDeploys, string webUrl)
         {
@@ -17,6 +25,26 @@ namespace Aetna.DevOps.Dashboard.UIWeb.Models
             Releasenotes = releaseNotes;
             ReleaseDeploys = releaseDeploys;
             WebUrl = webUrl;
+            Details = new Dictionary<string, string>();
+
+            string[] details = releaseNotes.Split(new string[] { ": ", ", ", " - " }, System.StringSplitOptions.RemoveEmptyEntries);
+            for (int i = 1; i < details.Length - 2; i++)
+            {
+                string[] datum = details[i].Split(new string[] { "=" }, System.StringSplitOptions.None);
+                Details.Add(datum[0], datum[1]);
+            }
+            if (details[0].Equals("Aetna-TFS-Info"))
+            {
+                ProjectSource = Source.TFS;
+            }
+            else if (Details.ContainsKey("ChangeSet"))
+            {
+                ProjectSource = Source.TcTFS;
+            }
+            else
+            {
+                ProjectSource = Source.TcGHE;
+            }
         }
     }
 }
